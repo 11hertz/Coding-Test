a00f9830a581db6daf148790c646db6393c82090
11hertz <reachzin@naver.com>
2023-10-25 01:41:04 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,14 @@
+function solution(num_list, n) {
+    var answer = [];
+    
+    for(let i = 0; i < num_list.length; i++) {
+       if(i % n === 0) {
+        answer.push(num_list.slice(i, i+n));
+       }
+    }
+    
+    
+    
+    
+    return answer;
+}

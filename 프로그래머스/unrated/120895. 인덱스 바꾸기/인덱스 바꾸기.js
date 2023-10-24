c0fb3dfe0c65f1e497a09667250d79d2e4e63b78
11hertz <reachzin@naver.com>
2023-10-25 01:37:23 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,13 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    
+    let arr = my_string.split('');
+    let temp = '';
+    
+    temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
+    
+    answer = arr.join('');
+    return answer;
+}

7622ad0b3173f5efe9aa42815af83042f957fa5e
11hertz <reachzin@naver.com>
2023-10-25 01:22:17 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,8 @@
+function solution(n, slicer, num_list) {
+    let [a, b, c] = slicer;
+    
+    return (n === 1) ? num_list.slice(0, b + 1) :
+             (n === 2) ? num_list.slice(a) :
+             (n === 3) ? num_list.slice(a, b + 1) : 
+             num_list.slice(a, b + 1).filter((_, i) => i % c - 1);  
+}

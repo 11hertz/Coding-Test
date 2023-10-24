a97f2929a1ed25d3f427c77683b9e66c85b09a6e
11hertz <reachzin@naver.com>
2023-10-25 01:19:01 +0900

    [level unrated] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let answer = [];
+    let len = my_string.length;
+    
+    for(let i = 0; i < len; i++) {
+        answer.push(my_string.slice(i));
+    }
+    
+    return answer.sort();
+}

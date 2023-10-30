2effaed4523bdfdbf45aedc797e52a0f06f9278e
11hertz <reachzin@naver.com>
2023-10-30 13:13:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.70 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -3,7 +3,7 @@ function solution(arr)
     const answer = [];
     const len = arr.length;
     let strLen = 0;
-    answer.push(arr[0])
+    answer.push(arr[0]);
     for(let i = 1; i < len; i++) {
         if(arr[i] === arr[i - 1]) { 
             continue;    

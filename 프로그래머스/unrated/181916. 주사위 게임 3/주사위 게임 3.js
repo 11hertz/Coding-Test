4d6d7d957527aa9a94931b885dcee5f0c9d56eca
11hertz <reachzin@naver.com>
2023-11-01 01:37:07 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -3,8 +3,6 @@ function solution(a, b, c, d) {
     
     const arr = [a, b, c, d].sort();    
  
-    console.log(arr);
-    
     if(arr[0] === arr[1] && arr[1] === arr[2] && arr[2] === arr[3]) {
         answer = 1111 * arr[0];   
     } else if(arr[0] === arr[1] && arr[1] === arr[2]) {

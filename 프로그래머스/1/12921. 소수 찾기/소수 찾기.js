7deeea932469cfa240dffc104068a2698be3f04a
11hertz <reachzin@naver.com>
2024-02-16 15:46:47 +0900

    [level 1] Title: 소수 찾기, Time: 149.28 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -6,8 +6,8 @@ function solution(n) {
     }
     
     for(let i = 2; i <= Math.sqrt(n); i++) {
-        for(let j = i + i; j <= n; j += i) {
-            if(arr[i] === 0) continue;
+        for(let j = i * 2; j <= n; j += i) {
+            if(arr[j] === 0) continue;
             arr[j] = 0;
         }
     }

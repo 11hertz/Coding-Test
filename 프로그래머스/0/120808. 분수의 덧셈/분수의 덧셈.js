64c26f437f857c1e00a6df0f90604b987b7cd60c
11hertz <reachzin@naver.com>
2024-02-21 20:24:59 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,32 +1,17 @@
 function solution(numer1, denom1, numer2, denom2) {
-    let max = Math.max(denom1, denom2);
-    let min = Math.min(denom1, denom2);
-    let commonDen = 0;
-    
-    for(let i = min; i <= (max * min); i += min) {
-        if(i % max === 0) {
-            commonDen = i;
-            break;
-        }
-    }
-    
-    let numer = (numer1 *= commonDen / denom1) + (numer2 *= commonDen / denom2);
-   
     const findGcd = (x, y) => {
-        if(x === y) return x;
-        let big = (x > y) ? x : y;
-        let small = (x > y) ? y : x;
-        
         let temp = 0; 
-        while(small !== 0) { 
-            temp = big % small; 
-            big = small; 
-            small = temp; 
+        while(y !== 0) { 
+            temp = x % y; 
+            x = y; 
+            y = temp; 
         } 
-        return big;   
+        return x;   
     }
 
-    const divider = findGcd(numer, commonDen);
+    const numer = denom1 * numer2 + denom2 * numer1;
+    const denom = denom1 * denom2;
+    const gcd = findGcd(numer, denom);
 
-    return [(numer/divider), (commonDen/divider)];
+    return [(numer/gcd), (denom/gcd)];
 }

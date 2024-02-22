a0720326bca5bc8e7c34fe5b0c54b2fef37d98fd
11hertz <reachzin@naver.com>
2024-02-23 01:47:13 +0900

    [level 1] Title: 시저 암호, Time: 1.08 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,11 @@
+function solution(s, n) {
+    const arr = s.split('').map(x => {
+        let str = x.charCodeAt();
+        if(str >= 65 && str <= 90) return String.fromCharCode((str + n - 65) % 26 + 65);
+        if(str >= 97 && str <= 122) return String.fromCharCode((str + n - 97) % 26 + 97);
+        
+        return ' ';
+    });
+
+    return arr.join('');
+}

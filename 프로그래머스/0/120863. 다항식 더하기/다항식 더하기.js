a7fd5c114d17fbd8b9a846445dd8ba45b20a67f8
11hertz <reachzin@naver.com>
2024-02-25 00:22:31 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,5 +1,4 @@
 function solution(polynomial) {
-    let answer = '';
     const xArr = polynomial.split(' + ').filter(x => x.includes('x'));
     const numArr = polynomial.split(' + ').filter(x => !x.includes('x'));
     
@@ -7,13 +6,10 @@ function solution(polynomial) {
     let numTerm = numArr.map(x => Number(x)).reduce((v, acc) => v + acc, 0);
 
     if(xTerm && numTerm) {
-        answer = (xTerm === 1) ? `x + ${numTerm}` : `${xTerm}x + ${numTerm}`;   
+        return (xTerm === 1) ? `x + ${numTerm}` : `${xTerm}x + ${numTerm}`;   
     } else if(xTerm && !numTerm) {
-        answer = (xTerm === 1) ? 'x' : `${xTerm}x`;   
+        return (xTerm === 1) ? 'x' : `${xTerm}x`;   
     } else if(!xTerm && numTerm) {
-        answer = `${numTerm}`;   
+        return `${numTerm}`;   
     } 
-    
-    
-    return answer;
 }

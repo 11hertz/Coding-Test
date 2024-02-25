cb11f4093d97e355a2db9fe6d23e66206328ef8b
11hertz <reachzin@naver.com>
2024-02-26 00:13:27 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,10 +1,7 @@
 function solution(n, lost, reserve) {
     let lostArr = lost.filter(x => !reserve.includes(x)).sort((a, b) => a - b);
     let reserveArr = reserve.filter(x => !lost.includes(x)).sort((a, b) => a - b);
-    
-    console.log(lostArr);
-    console.log(reserveArr);
-    
+
     for(let i = 0; i < lostArr.length; i++) {
         for(let j = 0; j < reserveArr.length; j++) {
             if(lostArr[i] - reserveArr[j] === 1 || lostArr[i] - reserveArr[j] === -1) {
@@ -13,7 +10,6 @@ function solution(n, lost, reserve) {
             }
         }
     }
-    console.log(lostArr.length);
-    
+
     return n - lostArr.length;
 }

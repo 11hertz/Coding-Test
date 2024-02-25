5b62f4aae18aee11e965b3c69b200bd87fc6c4a1
11hertz <reachzin@naver.com>
2024-02-26 00:30:24 +0900

    [level 1] Title: 예산, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,17 +1,14 @@
 function solution(d, budget) {
     let answer = 0;
     let sum = 0;
-    const arr = d.sort((a, b) => a - b);
-    for(let i = 0; i < arr.length; i++) {
-        sum += arr[i];
+    d.sort((a, b) => a - b);
+    for(let i = 0; i < d.length; i++) {
+        sum += d[i];
         answer = i + 1;
         if(sum > budget) {
             answer -= 1;
             break;
         }
     }
-    
-    console.log(sum);
-    
     return answer;
 }

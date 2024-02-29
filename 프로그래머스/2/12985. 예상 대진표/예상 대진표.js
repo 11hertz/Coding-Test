55ec8278d2ed30b32d62c3ba24d9edfbf7936b25
11hertz <reachzin@naver.com>
2024-02-29 16:24:07 +0900

    [level 2] Title: 예상 대진표, Time: 3.00 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,14 @@
+function solution(N, A, B)
+{
+    let answer = 0;
+    for(let i = 1; i <= 20; i++) {
+        A = (A % 2 === 0) ? (A / 2) : parseInt(A / 2) + 1;
+        B = (B % 2 === 0) ? (B / 2) : parseInt(B / 2) + 1;
+        console.log(A, B);
+        if(A === B) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}

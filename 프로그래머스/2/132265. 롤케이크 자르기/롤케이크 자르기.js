09e9bb6f4d9cb8ebcab86b9c15d70e327b3f580c
11hertz <reachzin@naver.com>
2024-03-08 14:53:43 +0900

    [level 2] Title: 롤케이크 자르기, Time: 181.28 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -11,10 +11,10 @@ function solution(topping) {
     
     for(let x of topping) {
         if(map1.size === map2.size) answer++;
-        if(map2.has(x)) {
-            map2.set(x, map2.get(x) - 1);
-            if(map2.get(x) === 0) map2.delete(x);
-        }
+        
+        map2.set(x, map2.get(x) - 1);
+        if(map2.get(x) === 0) map2.delete(x);
+        
         
         if(map1.has(x)) map1.set(x, map1.get(x) + 1);
         else map1.set(x, 1);

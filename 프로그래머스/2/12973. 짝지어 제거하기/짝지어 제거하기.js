5385f3c35ed880b115c8d5dc959775e2be30f178
11hertz <reachzin@naver.com>
2024-03-09 18:23:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.82 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -3,8 +3,8 @@ function solution(s)
     let stack = [];
     
     for(let i = 0; i < s.length; i++) {
-        if(stack.length > 0 && stack[stack.length -1] === s[i]) stack.pop();
+        if(stack[stack.length -1] === s[i]) stack.pop();
         else stack.push(s[i]);        
     }
-    return stack.length > 0 ? 0 : 1;
+    return (stack.length > 0) ? 0 : 1;
 }

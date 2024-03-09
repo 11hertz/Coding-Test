7e98157bd2a2826040de0e714229e345f02e16c5
11hertz <reachzin@naver.com>
2024-03-09 18:56:18 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -5,10 +5,7 @@ function solution(ingredient) {
     for(let i = 0; i < ingredient.length; i++) {
         stack.push(ingredient[i]);
         if(stack[stack.length - 4] === 1 && stack[stack.length - 3] === 2 && stack[stack.length - 2] === 3 && stack[stack.length - 1] === 1) {
-            stack.pop();
-            stack.pop();
-            stack.pop();
-            stack.pop();
+            stack.splice(-4);
             answer++;
         }
     }

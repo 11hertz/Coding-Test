da86601b475c05c0f2ed5b29f5eae64c3239af65
11hertz <reachzin@naver.com>
2024-03-11 12:02:27 +0900

    [level 1] Title: [1차] 다트 게임, Time: 4.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,24 @@
+function solution(dartResult) {
+    let arr = [];
+    
+    for(let i = 0; i < dartResult.length; i++) {   
+        if(arr[arr.length - 1] === '1' && dartResult[i] === '0') {
+            arr.pop();
+            arr.push(10);
+        } else if(!isNaN(dartResult[i])) arr.push(dartResult[i]);
+        if(dartResult[i] === 'S') {
+            arr[arr.length - 1] **= 1;
+        } else if(dartResult[i] === 'D') {
+            arr[arr.length - 1] **= 2;
+        } else if(dartResult[i] === 'T') {
+            arr[arr.length - 1] **= 3;
+        } else if(dartResult[i] === '*') {
+            arr[arr.length - 1] *= 2;
+            arr[arr.length - 2] *= 2;
+        } else if(dartResult[i] === '#') {
+            arr[arr.length - 1] *= -1;
+        }
+    }
+    console.log(arr);
+    return arr.reduce((v, acc) => v + acc, 0);
+}

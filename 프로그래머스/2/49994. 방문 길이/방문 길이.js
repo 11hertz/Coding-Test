0d0f7d639462235571f5785c428af816216759f7
11hertz <reachzin@naver.com>
2024-03-15 17:57:18 +0900

    [level 2] Title: 방문 길이, Time: 1.02 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,5 +1,4 @@
 function solution(dirs) {
-    let answer = 0;
     let dx = [1, 0, -1, 0];
     let dy = [0, 1, 0, -1];
     
@@ -12,27 +11,29 @@ function solution(dirs) {
         if(x === 'U') {
             if(currPoint[0] + dx[0] <= 5) {   
                 currPoint[0] += dx[0];
-                trail.push([String(cx) + String(cy), String(currPoint[0]) + String(currPoint[1])]);
+                trail.push([String(cx) + String(cy), 
+                            String(currPoint[0]) + String(currPoint[1])]);
             }
         } else if(x === 'R') {
             if(currPoint[1] + dy[1] <= 5) {
                 currPoint[1] += dy[1];
-                trail.push([String(cx) + String(cy), String(currPoint[0]) + String(currPoint[1])]);
+                trail.push([String(cx) + String(cy), 
+                            String(currPoint[0]) + String(currPoint[1])]);
             }
         } else if(x === 'D') {
             if(currPoint[0] + dx[2] >= -5) {
                 currPoint[0] += dx[2];
-                trail.push([String(cx) + String(cy), String(currPoint[0]) + String(currPoint[1])]);
+                trail.push([String(cx) + String(cy), 
+                            String(currPoint[0]) + String(currPoint[1])]);
             }
         } else if(x === 'L') {
             if(currPoint[1] + dy[3] >= -5) {
                 currPoint[1] += dy[3];
-                trail.push([String(cx) + String(cy), String(currPoint[0]) + String(currPoint[1])]);
+                trail.push([String(cx) + String(cy), 
+                            String(currPoint[0]) + String(currPoint[1])]);
             }
         }
     }
-    
-    answer = Array.from(new Set(trail.map(x => x.sort().toString()))).length;
-    
-    return answer;
+
+    return Array.from(new Set(trail.map(x => x.sort().toString()))).length;;
 }

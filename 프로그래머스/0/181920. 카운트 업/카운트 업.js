7e482e262587984aaf4bda6606da86c54f1c7ba1
11hertz <reachzin@naver.com>
2024-03-31 02:51:52 +0900

    [level 0] Title: 카운트 업, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,5 +1,3 @@
 function solution(start_num, end_num) {
-    
-    
-    return Array.from({length : end_num - start_num + 1}, () => start_num++);
+    return Array.from({ length : end_num - start_num + 1 }, () => start_num++);
 }

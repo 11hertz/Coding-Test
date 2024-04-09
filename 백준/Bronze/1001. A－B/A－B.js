92b573c53b1e15492b877f706090fa53527f3bd0
11hertz <reachzin@naver.com>
2024-04-10 06:37:47 +0900

    [Bronze V] Title: A-B, Time: 152 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,14 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [3, 2];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+	let [a, b] = input;
+    console.log(a - b);
+    process.exit();
+});

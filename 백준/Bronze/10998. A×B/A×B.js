ae82537710aa2c4a19369fd7f1e97cc3bea77f28
11hertz <reachzin@naver.com>
2024-04-11 05:58:01 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.js
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -1,14 +1,5 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-let input = [1, 2];
-
-readline.on('line', function(line) {
-    input = line.split(' ').map(el => parseInt(el));
-}).on('close', function(){
-	let [a, b] = input;
-    console.log(a * b);
-    process.exit();
-});
+let [a, b] = input;
+console.log(a * b);

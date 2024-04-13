93f06323092dbc7cfdaba75ce5c14e01efa25f8c
11hertz <reachzin@naver.com>
2024-04-13 12:11:55 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,13 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(x => x.replace('\r', ''));
+
+let repeatNum = input[0];
+
+for (let i = 1; i <= repeatNum; i++) {
+  let [a, b] = input[i].split(' ');
+  console.log(`Case #${i}: ${a} + ${b} = ${+a + +b}`);
+}

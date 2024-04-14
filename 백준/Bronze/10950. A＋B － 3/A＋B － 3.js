38b836eec9a2135fc0e5186f6d77e4ef85159f02
11hertz <reachzin@naver.com>
2024-04-14 12:42:18 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,14 +1,13 @@
-const fs = require('fs');
-const [n, ...input] = fs
-  .readFileSync('/dev/stdin')
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
   .toString()
   .trim()
   .split('\n')
-  .map(line => line.replace('\r', ''));
+  .map(x => x.split(' ').map(Number));
 
-let inputArr = input.map(x => x.split(' '));
+let t = input[0];
 
-for (let i = 0; i < n; i++) {
-  let [a, b] = inputArr[i];
-  console.log(+a + +b);
+for (let i = 1; i <= t; i++) {
+  let [a, b] = input[i];
+  console.log(a + b);
 }

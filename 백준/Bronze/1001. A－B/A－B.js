e98a6a381074db7e20dd2d88622952b94755c77b
11hertz <reachzin@naver.com>
2024-04-14 11:56:08 +0900

    [Bronze V] Title: A-B, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.js
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
 
 let [a, b] = input;
-console.log(+a - +b);
+console.log(a - b);

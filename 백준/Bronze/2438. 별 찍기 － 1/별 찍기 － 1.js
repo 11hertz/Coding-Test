d8e19b1b597e89e498b36f5e9531b2d047d92b5a
11hertz <reachzin@naver.com>
2024-04-14 12:55:46 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 9636 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -1,4 +1,7 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim();
 
 for (let i = 1; i <= input; i++) {
   let stars = '';

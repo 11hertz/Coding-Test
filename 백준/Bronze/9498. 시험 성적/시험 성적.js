080d84c2936abd22eff891f34f0f0bbdc4b209a0
11hertz <reachzin@naver.com>
2024-04-14 12:21:01 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,5 +1,7 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim();
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim();
 
 if (input >= 90 && input <= 100) console.log('A');
 else if (input >= 80 && input <= 89) console.log('B');

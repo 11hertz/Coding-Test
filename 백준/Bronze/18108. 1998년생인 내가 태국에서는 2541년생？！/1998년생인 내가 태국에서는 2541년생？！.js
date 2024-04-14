20c0b435ce1eed6d73e2fbad10acd5a7b792b562
11hertz <reachzin@naver.com>
2024-04-14 12:00:01 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -1,3 +1,7 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split(' ');
+
 console.log(+input - 543);

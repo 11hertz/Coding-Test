405f97e61b33990c67bc5ad888e3f0986bc19f93
11hertz <reachzin@naver.com>
2024-04-15 09:48:38 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 132 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,8 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(x => x.replace('\r', ''));
+
+for (let str of input) console.log(str);

993905e6011f6cb4922c0571b054a3c34c7a11a5
KIHOON LEE <reachzin@naver.com>
2024-04-24 02:22:25 +0900

    [Bronze V] Title: 직사각형, Time: 120 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,11 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+let A = input[0];
+let B = input[1];
+
+console.log(A * B);

bd8817a086ed937057a06c4e9df29fc007b964ec
KIHOON LEE <reachzin@naver.com>
2024-06-25 23:58:10 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -4,7 +4,7 @@ function solution(ingredient) {
     
     for(let i = 0; i < ingredient.length; i++) {
         stack.push(ingredient[i]);
-        if(stack[stack.length - 4] === 1 && stack[stack.length - 3] === 2 && stack[stack.length - 2] === 3 && stack[stack.length - 1] === 1) {
+        if(stack.at(-4) === 1 && stack.at(-3) === 2 && stack.at(-2) === 3 && stack.at(-1) === 1) {
             stack.splice(-4);
             answer++;
         }

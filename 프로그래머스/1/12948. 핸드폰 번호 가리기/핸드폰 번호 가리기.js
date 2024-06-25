a1b558cd0b2f93a4ad5369b1004bdd3f95f96e1f
KIHOON LEE <reachzin@naver.com>
2024-06-26 00:23:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,3 @@
 function solution(phone_number) {
-    let stars = [...phone_number.slice(0, -4)].map(x => "*").join('');
-    let numbers = phone_number.slice(-4);
-
-    return stars + numbers;  ;
+    return phone_number.replace(/\d(?=\d{4})/g, "*");
 }

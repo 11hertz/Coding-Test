3b02b696f1a7eac318111a954eb335395374b043
KIHOON LEE <reachzin@naver.com>
2024-07-02 00:06:51 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.16 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,9 +1,9 @@
 def solution(num, total):
     answer = []
     
-    for x in range(-101, 101):
+    for x in range(-100, 101):
         totalNum = sum(list(range(x, x + num)))
-        if totalNum == total : 
+        if totalNum == total: 
             answer = list(range(x, x + num))
             break
     
